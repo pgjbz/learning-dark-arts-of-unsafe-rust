413d2ba5401bacb9c4a5b70a5d43f223a6ff203b
Paulo Gabriel Justino Bezerra <pgjbzr@gmail.com>
2022-04-25 19:01:54 -0300

    grow function

--- vecnomicon/src/vecnomicon.rs
+++ vecnomicon/src/vecnomicon.rs
@@ -1,4 +1,9 @@
-use std::{marker::PhantomData, mem::size_of, ptr::NonNull};
+use std::{
+    alloc::{self, Layout},
+    marker::PhantomData,
+    mem::size_of,
+    ptr::NonNull,
+};
 
 pub struct VecNomicon<T> {
     data: NonNull<T>,
@@ -19,5 +24,32 @@ impl<T> VecNomicon<T> {
             _marker: PhantomData::default(),
             data: NonNull::dangling(),
         }
+    }
+
+    fn grow(&mut self) {
+        let zero_cap = self.cap == 0;
+        let (new_cap, new_layout) = if zero_cap {
+            (1, Layout::array::<T>(1).unwrap())
+        } else {
+            let new_cap = 1 * self.cap;
+            (new_cap, Layout::array::<T>(new_cap).unwrap())
+        };
+
+        assert!(
+            new_layout.size() <= isize::MAX as usize,
+            "allocation is too large"
+        );
+        let new_ptr = if zero_cap {
+            unsafe { alloc::alloc(new_layout) }
+        } else {
+            let old_layout = Layout::array::<T>(self.cap).unwrap();
+            let old_ptr = self.data.as_ptr() as *mut u8;
+            unsafe { alloc::realloc(old_ptr, old_layout, new_layout.size()) }
+        };
+        self.data = match NonNull::new(new_ptr as *mut T) {
+            Some(p) => p,
+            None => alloc::handle_alloc_error(new_layout),
+        };
+        self.cap = new_cap
     }
 }
